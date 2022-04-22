9252eea853080f1df05876be89ba28f7fbd7b6ba
geekmeba <mebratuhailu54@gmail.com>
2022-04-22 12:05:46 -0700

    edited main.h

--- main.h
+++ main.h
@@ -4,13 +4,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
- */
+
 typedef struct flags
 {
 	int plus;
@@ -18,12 +12,7 @@ typedef struct flags
 	int hash;
 } flags_t;
 
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
- */
+
 typedef struct printHandler
 {
 	char c;
